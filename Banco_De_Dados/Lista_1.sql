746636602a598284145de9976a0a71f0e2049f30
YuzoTaka <henrique.takahashi@sptech.school>
2022-03-08 18:47:14 -0300

    lista add

--- Banco_De_Dados/Lista_1.sql
+++ Banco_De_Dados/Lista_1.sql
@@ -3,240 +3,218 @@ CREATE DATABASE sprint1;
 USE sprint1;
 
 CREATE TABLE Atleta (
-	idAtleta INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(40),
-    modalidade varchar(40),
-    qtdMedalha INT
-);
+idAtleta CHAR(2) primary key,
+	nome VARCHAR(40),
+	modalidade VARCHAR(40),
+	qtdMedalha CHAR(2)
+	);
 
-INSERT INTO Atleta (nome, modalidade, qtdMedalha) VALUES 
-	('Fernando', 'Volei', 4),
-    ('Amanda', 'Skate', 6),
-    ('Carlos', 'Natação', 2),
-    ('Fernanda', 'Futebol', 5),
-    ('Mariana', 'Tenis', 4);
+INSERT INTO Atleta VALUES
+	('1', 'Donald', 'Atletismo', '7');
 
-DESC Atleta;
+INSERT INTO Atleta VALUES
+	('2', 'Douglas', 'Volei', '4'),
+    ('3', 'Miguel', 'Esgrima', '3'),
+    ('4', 'Pedro', 'Tiro', '3'),
+    ('5', 'Rafael', 'Tiro', '0');
 
 -- Exibir todos os dados da tabela.
 SELECT * FROM Atleta;
 
--- • Atualizar a quantidade de medalhas do atleta com id=1
-UPDATE Atleta SET qtdMedalha = 3 WHERE idAtleta = 1;
+-- Exibir apenas os nomes e quantidade de medalhas dos atletas.
+SELECT nome, qtdMedalha FROM Atleta;
 
--- Atualizar a quantidade de medalhas do atleta com id=2 e com o id=3
-UPDATE Atleta SET qtdMedalha = 5 WHERE idAtleta = 2 OR idAtleta = 3;
+-- Exibir apenas os dados dos atletas de uma determinada modalidade.
+SELECT * FROM Atleta WHERE modalidade = 'tiro';
 
--- Atualizar o nome do atleta com o id=4
-UPDATE Atleta SET nome = 'Rafael' WHERE idAtleta = 4;
+-- Exibir os dados da tabela ordenados pela modalidade.
+SELECT * FROM Atleta ORDER BY modalidade;
 
--- Adicionar o campo dtNasc na tabela, com a data de nascimento dos atletas, tipo date
-ALTER TABLE Atleta ADD COLUMN dtNasc DATE;
+-- Exibir os dados da tabela, ordenados pela quantidade de medalhas, em ordem decrescente
+SELECT * FROM Atleta ORDER BY qtdMedalha DESC;
 
--- Atualizar a data de nascimento de todos os atletas
-UPDATE Atleta SET dtNasc = '1995-06-11' WHERE idAtleta = 1;
-UPDATE Atleta SET dtNasc = '1993-01-01' WHERE idAtleta = 2;
-UPDATE Atleta SET dtNasc = '1984-04-16' WHERE idAtleta = 3;
-UPDATE Atleta SET dtNasc = '1980-12-11' WHERE idAtleta = 4;
-UPDATE Atleta SET dtNasc = '1971-01-13' WHERE idAtleta = 5;
+-- Exibir os dados da tabela, dos atletas cujo nome contenha a letra s
+SELECT * FROM Atleta WHERE nome LIKE '%s%';
 
--- Excluir o atleta com o id=5
-DELETE FROM Atleta WHERE idAtleta = 5;
+-- Exibir os dados da tabela, dos atletas cujo nome comece com uma determinada letra
+SELECT * FROM Atleta WHERE nome LIKE 'd%';
 
--- Exibir os atletas onde a modalidade é diferente de natação
-SELECT * FROM Atleta WHERE modalidade <> 'Natação';
+-- Exibir os dados da tabela, dos atletas cujo nome termine com a letra o
+SELECT * FROM Atleta WHERE nome LIKE '%o';
 
--- Exibir os dados dos atletas que tem a quantidade de medalhas maior ou igual a 3
-SELECT * FROM Atleta WHERE qtdMedalha >= 3;
+-- Exibir os dados da tabela, dos atletas cujo nome tenha a penúltima letra r
+SELECT * FROM Atleta WHERE nome LIKE '%r_';
 
--- Modificar o campo modalidade do tamanho 40 para o tamanho 60
-ALTER TABLE Atleta MODIFY COLUMN modalidade VARCHAR(60);
+-- Eliminar a tabela
+DROP TABLE Atleta;
 
--- Descrever os campos da tabela mostrando a atualização do campo modalidade
-DESC Atleta;
-
--- Limpar os dados da tabela
-TRUNCATE TABLE Atleta;
 
 
 CREATE TABLE Musica(
-	idMusica CHAR(2) PRIMARY KEY,
-    titulo VARCHAR(40),
-    artista VARCHAR(40),
-    genero VARCHAR(40)
+	idMusica CHAR  (2) primary key,
+    titulo VARCHAR (40),
+    artista VARCHAR (40),
+    genero VARCHAR (40)
 );
 
 INSERT INTO Musica VALUES
-(1, 'Shape of You', 'Ed Sheeran', 'POP'),
-(2, 'Cedo ou tarde', 'NXZero', 'Gospel'),
-(3, 'Homura', 'LiSA', 'JPOP'),
-(4, 'Butter', 'BTS', 'KPOP'),
-(5, 'Sugar', 'Marrom 5', 'POP'),
-(6, 'Stay', 'Justin Bieber', 'POP'),
-(7, 'Não temos tempo', 'Nossa toca','POP');
+	('1', 'Firework', 'Katy Perry', 'POP'),
+    ('2', 'Kaibutsu', 'Yoasobi', 'JPOP'),
+    ('3', 'We will rock you', 'Rolling Stones', 'Rock'),
+    ('4', 'Homura', 'LiSA', 'JPOP'),
+    ('5', 'Shake it off', 'Taylor Swift', 'POP');
+    
+INSERT INTO Musica VALUES
+	('6', 'Love Story', 'Taylor Swift', 'POP');
 
--- Exibir todos os dados da tabela.
+--  Exibir todos os dados da tabela
 SELECT * FROM Musica;
 
--- Adicionar o campo curtidas do tipo int na tabela
-ALTER TABLE Musica ADD COLUMN curtidas INT;
+-- Exibir apenas os títulos e os artistas das músicas.
+SELECT titulo, artista FROM musica;
 
--- Atualizar o campo curtidas de todas as músicas inseridas
-UPDATE Musica SET curtidas = 500 WHERE idMusica = 1;
-UPDATE Musica SET curtidas = 500 WHERE idMusica = 2;
-UPDATE Musica SET curtidas = 700 WHERE idMusica = 3;
-UPDATE Musica SET curtidas = 800 WHERE idMusica = 4;
-UPDATE Musica SET curtidas = 900 WHERE idMusica = 5;
-UPDATE Musica SET curtidas = 600 WHERE idMusica = 6;
-UPDATE Musica SET curtidas = 800 WHERE idMusica = 7;
+-- Exibir apenas os dados das músicas de um determinado gênero.
+SELECT * FROM musica WHERE genero = 'JPOP';
 
--- Modificar o campo artista do tamanho 40 para o tamanho 80
-ALTER TABLE Musica MODIFY COLUMN artista VARCHAR(80);
+-- Exibir apenas os dados das músicas de um determinado artista.
+SELECT * FROM musica WHERE artista = 'Taylor Swift';
 
--- Atualizar a quantidade de curtidas da música com id=1
-UPDATE Musica SET curtidas = 400 WHERE idMusica = 1;
+-- Exibir os dados da tabela ordenados pelo título da música.
+SELECT * FROM musica ORDER BY titulo;
 
--- Atualizar a quantidade de curtidas das músicas com id=2 e com o id=3
-UPDATE Musica SET curtidas = 550 WHERE idMusica in (2, 3);
+-- Exibir os dados da tabela ordenados pelo artista em ordem decrescente.
+SELECT * FROM musica ORDER BY artista DESC;
 
--- Atualizar o nome da música com o id=5
-UPDATE Musica SET titulo = 'PayPhone' WHERE idMusica = 5;
+-- Exibir os dados da tabela, das músicas cujo título comece com uma determinada letra.
+SELECT * FROM musica WHERE titulo like 'k%';
 
--- Excluir a música com o id=4
-DELETE FROM Musica WHERE idMusica = 4;
+-- Exibir os dados da tabela, das músicas cujo artista termine com uma determinada letra.
+SELECT * FROM musica WHERE artista like 'L%';
 
--- Exibir as músicas onde o gênero é diferente de funk
-SELECT * FROM Musica WHERE genero <> 'funk';
+-- Exibir os dados da tabela, das músicas cujo gênero tenha como segunda letra uma determinada letra
+SELECT * FROM musica WHERE genero like '_o%';
 
--- Exibir os dados das músicas que tem curtidas maior ou igual a 20
-SELECT * FROM Musica WHERE curtidas >= 20;
+-- Exibir os dados da tabela, das músicas cujo título tenha como penúltima letra uma determinada letra.
+SELECT * FROM musica WHERE titulo like '%r_';
 
--- Descrever os campos da tabela mostrando a atualização do campo artista
-DESC Musica;
-
--- Limpar os dados da tabela
-TRUNCATE Musica;
+-- Elimine a tabela.
+DROP TABLE musica;
 
 
-CREATE TABLE Filme (
-	idFilme CHAR(2) PRIMARY KEY,
-    titulo VARCHAR(50),
+
+CREATE TABLE Filme(
+	idFilme CHAR(2) primary key,
+    título VARCHAR(50),
     genero VARCHAR(40),
     diretor VARCHAR(40)
 );
 
-
 INSERT INTO Filme VALUES
-(1, 'SpiderMan No way home', '	Ação', 'Jon Watts'),
-(2, 'Gasoline Alley', 'Suspense', 'Edward Drake'),
-(3, 'Pânico', 'Terror', 'Tyler Gillett'),
-(4,'The in between', 'Romance', 'Arie Posin'),
-(5,'The wolf and the Lion', 'Drama', 'Gilles de Maistre'),
-(6, 'O massacre da serra elétrica', 'Terror', 'David Blue Garcia'),
-(7, 'Peter`s To Do List', 'Ação', 'Jon Watts');
+('1', 'Avatar', 'sci-fi', 'James Cameron'),
+('2', 'Vingadores: Ultimato', 'Ação', 'Joe Russo'),
+('3', 'Titanic', 'romance', 'James Cameron'),
+('4', 'Star wars, O despertar da força', 'Sci-fi', 'J.J. Abrams'),
+('5', 'O Rei Leão', 'Animação', 'Roger Allers'),
+('6', 'Os Vingadores', 'Aventura', 'Joss Whedon'),
+('7', 'O exorcista', 'Terror', 'William Friedkin');
 
--- Exibir todos os dados da tabela
+UPDATE Filme SET genero = 'Sci-fi' WHERE idFilme = '1';
+
+-- Exibir todos os dados da tabela.
 SELECT * FROM Filme;
 
--- Adicionar o campo protagonista do tipo varchar(50) na tabela
-ALTER TABLE Filme ADD protagonista VARCHAR(50);
+-- Exibir apenas os títulos e os diretores dos filmes
+SELECT título, diretor FROM Filme;
 
--- Atualizar o campo protagonista de todas os filmes inseridos
-UPDATE Filme SET protagonista = 'Spider man' WHERE idFilme = 1;
-UPDATE Filme SET protagonista = 'Bruce Willis' WHERE idFilme = 2;
-UPDATE Filme SET protagonista = 'Neve Campbell' WHERE idFilme = 3;
-UPDATE Filme SET protagonista = 'Joey King' WHERE idFilme = 4;
-UPDATE Filme SET protagonista = 'Graham Greene' WHERE idFilme = 5;
-UPDATE Filme SET protagonista = 'Sarah Yarkin' WHERE idFilme = 6;
-UPDATE Filme SET protagonista = 'Spider man' WHERE idFilme = 7;
+-- Exibir apenas os dados dos filmes de um determinado gênero
+SELECT * FROM Filme WHERE genero = 'Sci-fi';
 
--- Modificar o campo diretor do tamanho 40 para o tamanho 150
-ALTER TABLE Filme MODIFY COLUMN diretor VARCHAR(150);
+-- Exibir apenas os dados dos filmes de um determinado diretor
+SELECT * FROM Filme WHERE diretor = 'James Cameron';
 
--- Atualizar o diretor do filme com id=5
-UPDATE Filme SET diretor = 'Steven Spielberg' WHERE idFilme = 5;
+-- Exibir os dados da tabela ordenados pelo título do filme
+SELECT * FROM Filme ORDER BY título;
 
--- Atualizar o diretor dos filmes com id=2 e com o id=7
-UPDATE Filme SET diretor = 'Stanley Kubrick' WHERE idFilme in (2, 7);
+-- Exibir os dados da tabela ordenados pelo diretor em ordem decrescente
+SELECT * FROM Filme ORDER BY diretor DESC;
 
--- Atualizar o título do filme com o id=6
-UPDATE Filme SET titulo = 'Tejano' WHERE idFilme = 6;
+-- Exibir os dados da tabela, dos filmes cujo título comece com uma determinada letra.
+SELECT * FROM Filme WHERE título like 'o%';
 
--- Excluir o filme com o id=3
-DELETE FROM Filme WHERE idFilme = 3;
+-- Exibir os dados da tabela, dos filmes cujo artista termine com uma determinada letra.
 
--- Exibir os filmes em que o gênero é diferente de drama
-SELECT * FROM Filme WHERE genero <> 'drama';
+SELECT * FROM Filme WHERE diretor like '%o';
 
--- Exibir os dados dos filmes que o gênero é igual ‘suspense
-SELECT * FROM Filme WHERE genero = 'suspense';
+-- Exibir os dados da tabela, dos filmes cujo gênero tenha como segunda letra uma determinada letra.
+SELECT * FROM Filme WHERE genero like '_e%';
 
--- Descrever os campos da tabela mostrando a atualização do campo protagonista e diretor;
-DESC Filme;
+-- Exibir os dados da tabela, dos filmes cujo título tenha como penúltima letra uma determinada letra.
+SELECT * FROM Filme WHERE título like '%e_';
 
--- Limpar os dados da tabela
-TRUNCATE Filme;
+-- Elimine a tabela.
+DROP TABLE Filme;
+
+
 
 
 CREATE TABLE Professor (
-	idProfessor CHAR(2) PRIMARY KEY,
+	idProfessor CHAR(1) primary key,
     nome VARCHAR(50),
     especialidade VARCHAR(40),
     dtNasc DATE
 );
 
 INSERT INTO Professor VALUES
-	(1, 'Gabriel', 'Banco de Dados', '1980-10-15'),
-    (2, 'Ricardo', 'TI', '1960-06-11'),
-    (3, 'Roberta', 'Arq Comp', '1999-05-08'),
-    (4, 'Bruna', 'Banco de Dados', '1991-04-14'),
-    (5, 'Edson', 'PI', '1993-12-31');
+ ('1', 'Vivian', 'Banco de Dados', '2003-02-17'),
+ ('2', 'Frizza', 'Algoritmo', '2003-03-22'),
+ ('3', 'Thiago', 'TI', '2003-04-10'),
+ ('4', 'Kaline', 'Socio', '2003-05-08'),
+ ('5', 'Thiago', 'Socio', '2003-06-25'),
+ ('6', 'Eduardo', 'ArqComp', '2003-07-30');
 
--- Exibir todos os dados da tabela
+
+-- Exibir todos os dados da tabela.
 SELECT * FROM Professor;
 
--- Adicionar o campo funcao do tipo varchar(50), onde a função só pode ser ‘monitor’, ‘assistente’ ou ‘titular’
-ALTER TABLE Professor ADD Função VARCHAR(50);
-ALTER TABLE Professor ADD CHECK (função = 'monitor' or função = 'assistente' or função = 'títular');
+-- Exibir apenas as especialidades dos professores.
+SELECT especialidade FROM Professor;
 
--- Atualizar os professores inseridos e suas respectivas funções
-UPDATE Professor SET função = 'monitor' WHERE idProfessor = 1;
-UPDATE Professor SET função = 'assistente' WHERE idProfessor = 2;
-UPDATE Professor SET função = 'monitor' WHERE idProfessor = 3;
-UPDATE Professor SET função = 'assistente' WHERE idProfessor = 4;
-UPDATE Professor SET função = 'títular' WHERE idProfessor = 5;
+-- Exibir apenas os dados dos professores de uma determinada especialidade.
+SELECT * FROM Professor WHERE especialidade = 'Socio';
 
--- Inserir um novo professor
-INSERT INTO Professor VALUES
-(6, 'Claudio', 'TI', '1990-02-16', 'títular');
+-- Exibir os dados da tabela ordenados pela data de nascimento do professor em ordem decrescente.
+SELECT * FROM Professor ORDER BY dtNasc DESC;
 
--- Excluir o professor onde o idProfessor é igual a 5
-DELETE FROM Professor WHERE idProfessor = 5;
+-- Exibir os dados da tabela, dos professores cujo nome comece com uma determinada letra.
+SELECT * FROM Professor WHERE nome LIKE 'v%';
 
--- Exibir apenas os nomes dos professores titulares
-SELECT nome FROM Professor WHERE função LIKE 'títular';
+-- Exibir os dados da tabela, dos professores cujo nome termine com uma determinada letra
+SELECT * FROM Professor WHERE nome LIKE '%o';
 
--- Exibir apenas as especialidades e as datas de nascimento dos professores monitores
-SELECT especialidade, dtNasc FROM Professor WHERE função LIKE 'monitor';
+-- Exibir os dados da tabela, dos professores cujo nome tenha como segunda letra uma determinada letra.
+SELECT * FROM Professor WHERE nome LIKE '_a%';
 
--- Atualizar a data de nascimento do idProfessor igual a 3
-UPDATE Professor SET dtNasc = '2002-06-11' WHERE idProfessor = 3;
+-- Exibir os dados da tabela, dos professores cujo nome tenha como penúltima letra uma determinada letra.
+SELECT * FROM Professor WHERE nome LIKE '%z_';
 
--- Limpar a tabela Professor
-TRUNCATE Professor;
+-- Elimine a tabela.
+DROP TABLE Professor;
 
 
-CREATE TABLE Curso (
-	idCurso CHAR(2) PRIMARY KEY,
+
+
+CREATE TABLE Curso(
+	idCurso CHAR(2) primary key,
     nome VARCHAR(50),
     sigla VARCHAR(3),
-    coordenador VARCHAR(50)
+    coordenador VARCHAR(30)
 );
 
+
 INSERT INTO Curso VALUES
-(1, 'Analise e Desenvolvimento de Sistema', 'ADS', 'Gerson'),
-(2, 'Sistema de Informaçao', 'SSI', 'Fernando'),
-(3, 'Ciencias da Computaçao', 'CCO', 'Claudia');
+	('1', 'Análise e desenvolvimento de sistema', 'ADS', 'Gerson'),
+	('2', 'Sistema de informação', 'SSI', 'Jonas'),
+    ('3', 'Ciência da computação', 'CCO', 'Pedro');
 
 
 -- Exibir todos os dados da tabela.
@@ -323,4 +301,16 @@ DROP periodicidade;
 
 -- EXCLUIR O BANCO DE DADOS SPRINT1*
 DROP DATABASE sprint1;
+
+
+
+
+
+
+
+
+
+
+
+
 
