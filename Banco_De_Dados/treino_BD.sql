f4537f64296dfd522efbbb1712632c819a46108a
YuzoTaka <henrique.takahashi@sptech.school>
2022-03-22 19:13:16 -0300

    teste bd concluido

--- Banco_De_Dados/treino_BD.sql
+++ Banco_De_Dados/treino_BD.sql
@@ -57,10 +57,43 @@ INSERT INTO Campeao(nome, funcao) VALUES
 ALTER TABLE Campeao ADD COLUMN fkRegiao INT;
 ALTER TABLE Campeao ADD FOREIGN KEY (fkRegiao) REFERENCES Runeterra(idRuneterra);
 
+UPDATE Campeao SET fkRegiao = 14 WHERE idCampeao = 1;
+UPDATE Campeao SET fkRegiao = 2 WHERE idCampeao = 2;
+UPDATE Campeao SET fkRegiao = 2 WHERE idCampeao = 3;
+UPDATE Campeao SET fkRegiao = 12 WHERE idCampeao = 4;
+UPDATE Campeao SET fkRegiao = 14 WHERE idCampeao = 5;
+UPDATE Campeao SET fkRegiao = 12 WHERE idCampeao = 6;
+UPDATE Campeao SET fkRegiao = 9 WHERE idCampeao = 7;
+UPDATE Campeao SET fkRegiao = 11 WHERE idCampeao = 8;
+UPDATE Campeao SET fkRegiao = 12 WHERE idCampeao = 9;
+UPDATE Campeao SET fkRegiao = 9 WHERE idCampeao = 10;
+UPDATE Campeao SET fkRegiao = 3 WHERE idCampeao = 11;
+UPDATE Campeao SET fkRegiao = 5 WHERE idCampeao = 12;
+UPDATE Campeao SET fkRegiao = 9 WHERE idCampeao = 13;
+UPDATE Campeao SET fkRegiao = 6 WHERE idCampeao = 14;
+UPDATE Campeao SET fkRegiao = 5 WHERE idCampeao = 15;
+UPDATE Campeao SET fkRegiao = 6 WHERE idCampeao = 16;
+UPDATE Campeao SET fkRegiao = 3 WHERE idCampeao = 17;
+UPDATE Campeao SET fkRegiao = 3 WHERE idCampeao = 18;
+UPDATE Campeao SET fkRegiao = 2 WHERE idCampeao = 19;
+UPDATE Campeao SET fkRegiao = 2 WHERE idCampeao = 20;
+UPDATE Campeao SET fkRegiao = 2 WHERE idCampeao = 21;
+UPDATE Campeao SET fkRegiao = 2 WHERE idCampeao = 22;
+UPDATE Campeao SET fkRegiao = 6 WHERE idCampeao = 23;
+
+
+SELECT Campeao.nome, Runeterra.regiao FROM Campeao JOIN Runeterra ON Runeterra.idRuneterra = Campeao.idCampeao;
+
+SELECT Campeao.*, Runeterra.regiao FROM Campeao JOIN Runeterra ON Runeterra.idRuneterra = Campeao.idCampeao;
+
+SELECT Campeao.nome, Campeao.funcao, Runeterra.regiao FROM Campeao JOIN Runeterra ON Runeterra.idRuneterra = Campeao.idCampeao WHERE funcao = 'mago';
+
 
 
 SELECT * FROM Runeterra;
-SELECT * FROM Campeao;
+SELECT * FROM Campeao; 
+
+
 
 DESC Campeao;
 
